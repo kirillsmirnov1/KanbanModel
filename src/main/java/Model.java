58734e1d590b872f0e914b98763c1309f65d8e53
trulden <thegaldren@gmail.com>
2018-12-03 12:42:32 +0300

    Доделал внешний цикл, сделал внутренний цикл

--- src/main/java/Model.java
+++ src/main/java/Model.java
@@ -50,17 +50,99 @@ public class Model {
 
     // Внешний цикл
     void outerCycle(){
+        // Подготовка цикла
         productivityLevel = 1d;
         fillBacklog();
+        //printStages();
         System.out.println("\nIn backlog: ");
         stages.get(StageType.BACKLOG).printTasks();
         Stream.of(workers).forEach(Worker::refillEnergy);
 //        Stream.of(workers).forEach(w -> System.out.print(w.getEnergy() + " "));
 //        Stream.of(workers).forEach(w -> w.deductEnergy(2));
 //        Stream.of(workers).forEach(w -> System.out.print(w.getEnergy() + " "));
-        // TODO закончить внешний цикл
+        while(productivityLevel > 0d && workersHaveEnergy()){ // TODO пересчет энергии работников
+            if(!innerCycle())
+                productivityLevel -= 0.05d;
+        }
+    }
 
-        // TODO сделать внутренний цикл
+    private boolean workersHaveEnergy() {
+        for(Worker worker : workers){
+            if(worker.getEnergy() > 0)
+                return true;
+        }
+        return false;
+    }
+
+    private boolean innerCycle() {
+        int workDoneAtThisCycle;
+        boolean tasksMoved;
+        workDoneAtThisCycle = makeSomeWork();
+        tasksMoved = moveTasks();
+        return ((workDoneAtThisCycle != 0) || tasksMoved);
+    }
+
+    private boolean moveTasks() {
+        boolean tasksMoved = false;
+        for(StageType stage : StageType.stagesReverse){
+            switch (stage){
+                case DEPLOYMENT:
+                    break;
+                default:
+                    for(Task task : stages.get(stage).getTasksToRemove()){
+                        if(task != null){
+                        if(stages.get(task.getNextStage()).canAddTask()){
+                            stages.get(stage).removeTask(task);
+                            stages.get(task.getNextStage()).addTask(task);
+                            task.moveToNextStage();
+                            tasksMoved = true;
+                        }
+                        }
+                    }
+            }
+        }
+        return tasksMoved;
+    }
+
+    private int makeSomeWork() {
+        int amountOfWork = 0;
+        for(StageType stage : StageType.stagesReverse){ // TODO заменить на проход по рабочим стадиям
+                switch (stage){
+                    case BACKLOG:
+                    case DEPLOYMENT:
+                        break;
+                    default:
+                        if(stages.get(stage).getNumberOfTasks() !=0){
+                            for(Task task : ((StageWorking)stages.get(stage)).getTasksInWork()){
+                                for(Worker worker : workers){
+                                    // Смотрю сколько в таске осталось работы
+                                    int taskCanTake   = task.getResumingWorkAtCurrentStage();
+                                    if(taskCanTake > 0 // Если еще есть
+                                            && worker.getEnergy() > 0 // И у работника есть силы
+                                            && worker.getProductivityAtStage(stage) >= productivityLevel){ // И он достаточно компетентен
+
+                                        //Считаю сколько он может наработать
+                                        int workerCanGive = (int) (worker.getEnergy() * worker.getProductivityAtStage(stage)); // TODO округление в большую сторону
+
+                                        //Выполняю возможное количество работы
+                                        if(workerCanGive >= taskCanTake){
+                                            worker.deductEnergy((int)((double)taskCanTake / worker.getProductivityAtStage(stage))); // TODO округление в меньшую сторону
+                                            task.makeSomeWork(taskCanTake);
+                                            amountOfWork += taskCanTake;
+                                            ((StageWorking)stages.get(stage)).moveTaskToFinished(task); // FIXME Тут может быть порблема с перемещением.
+                                        } else {
+                                            worker.deductEnergy(workerCanGive);
+                                            task.makeSomeWork(workerCanGive);
+                                            amountOfWork += workerCanGive;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                }
+        }
+
+        return amountOfWork;
     }
 
     // Заполнение бэклога
