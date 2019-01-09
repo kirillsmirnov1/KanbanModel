6cfd599cca9a7c048dfec9748b6f40c7ff75fb92
trulden <thegaldren@gmail.com>
2019-01-09 12:27:09 +0300

    Однострочный вызов sleep. Без этого файла прошлый коммит не соберется.

---
+++ src/main/java/trulden/com/vk/KanbanModel/util/Util.java
@@ -0,0 +1,11 @@
+package trulden.com.vk.KanbanModel.util;
+
+import java.util.concurrent.TimeUnit;
+
+public class Util {
+    public static void sleepMilliseconds(int millisecondsToSleep){
+        try{
+            TimeUnit.MILLISECONDS.sleep(millisecondsToSleep);
+        } catch (InterruptedException e){}
+    }
+}
