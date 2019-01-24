ca509a09fd27204593834aa97b3b55fe2dee4b22
trulden <thegaldren@gmail.com>
2019-01-24 18:10:30 +0300

    Обновление CFD

--- src/main/java/trulden/com/vk/KanbanModel/view/CFDController.java
+++ src/main/java/trulden/com/vk/KanbanModel/view/CFDController.java
@@ -1,9 +1,9 @@
 package trulden.com.vk.KanbanModel.view;
 
+import javafx.application.Platform;
 import javafx.beans.property.IntegerProperty;
 import javafx.fxml.FXML;
 import javafx.scene.chart.AreaChart;
-import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 
 import java.util.HashMap;
@@ -16,7 +16,6 @@ public class CFDController {
     AreaChart CFDChart;
 
     public void setDayTracking(IntegerProperty currentDay, HashMap<Integer, int[]> CFD) {
-
         int numberOfSeries = CFD.get(0).length;
 
         CFDSeries = new XYChart.Series[numberOfSeries];
@@ -28,11 +27,20 @@ public class CFDController {
             CFDChart.getData().addAll(CFDSeries[i]);
         }
 
+        // Начальное заполнение столбцов
         for(int day=0; day < CFD.size(); ++day){
             int[] CFDForToday = CFD.get(day);
             for(int seriesIterator = 0; seriesIterator < numberOfSeries; ++seriesIterator){
                 CFDSeries[seriesIterator].getData().add(new XYChart.Data(day, CFDForToday[seriesIterator]));
             }
         }
+
+        // Установка слежки за изменением дня
+        currentDay.addListener((observable, oldValue, newValue) -> Platform.runLater(() -> {
+            int[] CFDForToday = CFD.get(newValue.intValue()-1); // TODO это норм что я тут вычитаю?
+            for(int seriesIterator=0; seriesIterator < numberOfSeries; ++seriesIterator){
+                CFDSeries[seriesIterator].getData().add(new XYChart.Data(newValue, CFDForToday[seriesIterator]));
+            }
+        }));
     }
 }
