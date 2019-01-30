182c91463b30d54dfa14078e9415e089d6c83240
trulden <thegaldren@gmail.com>
2019-01-30 14:33:39 +0300

    Внезапно понял что не знаю как все это сейчас делать Пока буду описывать сценариями то, что уже реализовано

--- src/main/java/trulden/com/vk/KanbanModel/util/Scenario.java
+++ src/main/java/trulden/com/vk/KanbanModel/util/Scenario.java
@@ -2,24 +2,16 @@ package trulden.com.vk.KanbanModel.util;
 
 
 public class Scenario {
-    private boolean allRoundEmployees   = true;     // TODO
+    //private boolean allRoundEmployees   = true;     // TODO
     private boolean linearTasksMovement = true;     // TODO
-    private boolean workersNeedToRest   = false;    // TODO
-    private boolean performCardsFromRightToLeft = true; // TODO
+    //private boolean workersNeedToRest   = false;    // TODO
+    //private boolean performCardsFromRightToLeft = true; // TODO
+
+    //private CardPriority cardPriority = CardPriority.OLD; // TODO
 
     private int deploymentFrequency = 7; // TODO
 
-    private CardPriority cardPriority = CardPriority.OLD; // TODO
-
     // Зона геттеров и сеттеров
-
-    public boolean isAllRoundEmployees() {
-        return allRoundEmployees;
-    }
-
-    public void setAllRoundEmployees(boolean allRoundEmployees) {
-        this.allRoundEmployees = allRoundEmployees;
-    }
 
     public boolean isLinearTasksMovement() {
         return linearTasksMovement;
@@ -27,22 +19,6 @@ public class Scenario {
 
     public void setLinearTasksMovement(boolean linearTasksMovement) {
         this.linearTasksMovement = linearTasksMovement;
-    }
-
-    public boolean isPerformCardsFromRightToLeft() {
-        return performCardsFromRightToLeft;
-    }
-
-    public void setPerformCardsFromRightToLeft(boolean performCardsFromRightToLeft) {
-        this.performCardsFromRightToLeft = performCardsFromRightToLeft;
-    }
-
-    public CardPriority getCardPriority() {
-        return cardPriority;
-    }
-
-    public void setCardPriority(CardPriority cardPriority) {
-        this.cardPriority = cardPriority;
     }
 
     public int getDeploymentFrequency() {
@@ -53,19 +29,11 @@ public class Scenario {
         this.deploymentFrequency = deploymentFrequency;
     }
 
-    public boolean isWorkersNeedToRest() {
-        return workersNeedToRest;
-    }
-
-    public void setWorkersNeedToRest(boolean workersNeedToRest) {
-        this.workersNeedToRest = workersNeedToRest;
-    }
-
     // Приоритет выбора карточки для выполнения
 
-    public enum  CardPriority {
-        OLD,    // Самая старая
-        EASY,   // Самая лёгкая
-        HARD    // Самая трудная
-    }
+//    public enum  CardPriority {
+//        OLD,    // Самая старая
+//        EASY,   // Самая лёгкая
+//        HARD    // Самая трудная
+//    }
 }
