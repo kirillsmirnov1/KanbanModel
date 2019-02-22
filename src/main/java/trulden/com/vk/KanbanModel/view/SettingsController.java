69b35746d68481d62bd8bcfce268a844cbf722e4
trulden <thegaldren@gmail.com>
2019-02-22 12:43:31 +0300

    Добавил в контроллер формы ссылки на элементы

--- src/main/java/trulden/com/vk/KanbanModel/view/SettingsController.java
+++ src/main/java/trulden/com/vk/KanbanModel/view/SettingsController.java
@@ -1,4 +1,24 @@
 package trulden.com.vk.KanbanModel.view;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+
 public class SettingsController {
+
+    @FXML
+    TextField numberOfDaysTextField;
+    @FXML
+    TextField numberOfWorkersTextField;
+    @FXML
+    TextField UISleepTimeTextField;
+    @FXML
+    TextField scenariosPathTextField;
+    @FXML
+    CheckBox showBoardCheckBox;
+    @FXML
+    Button startModelButton;
+    @FXML
+    Button resultsGraphicButton;
 }
