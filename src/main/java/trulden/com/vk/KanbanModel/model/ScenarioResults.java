d6060f773e8eb37856480ed84becbbde6b767aa0
trulden <thegaldren@gmail.com>
2019-02-28 14:28:33 +0300

    Комментировал ScenarioResults

--- src/main/java/trulden/com/vk/KanbanModel/model/ScenarioResults.java
+++ src/main/java/trulden/com/vk/KanbanModel/model/ScenarioResults.java
@@ -1,9 +1,19 @@
 package trulden.com.vk.KanbanModel.model;
 
+// Результаты выполнения сценария
 public class ScenarioResults {
+    // Время тасок на доске
     private final double leadTime;
+    // Время тасок на рабочих стадиях
     private final double cycleTime;
+    // Количество завершенных задач
     private final int    tasksFinished;
+
+    ScenarioResults(double leadTime, double cycleTime, int tasksFinished){
+        this.leadTime = leadTime;
+        this.cycleTime = cycleTime;
+        this.tasksFinished = tasksFinished;
+    }
 
     public double getLeadTime() {
         return leadTime;
@@ -15,11 +25,5 @@ public class ScenarioResults {
 
     public int getTasksFinished() {
         return tasksFinished;
-    }
-
-    ScenarioResults(double leadTime, double cycleTime, int tasksFinished){
-        this.leadTime = leadTime;
-        this.cycleTime = cycleTime;
-        this.tasksFinished = tasksFinished;
     }
 }
