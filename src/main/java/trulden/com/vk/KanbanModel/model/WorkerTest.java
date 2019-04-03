a8128f1d7304ae0bf8bb3b9f8e2a000780a51cc6
trulden <thegaldren@gmail.com>
2019-04-03 16:24:17 +0300

    add WorkerTest::testSetLastTask()

--- src/main/java/trulden/com/vk/KanbanModel/model/WorkerTest.java
+++ src/main/java/trulden/com/vk/KanbanModel/model/WorkerTest.java
@@ -3,6 +3,9 @@ package trulden.com.vk.KanbanModel.model;
 import org.junit.Rule;
 import org.junit.jupiter.api.*;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import static org.junit.Assert.assertEquals;
 
 class WorkerTest {
@@ -26,5 +29,17 @@ class WorkerTest {
                     Worker.generateRandomWorker("");
                 }
         );
+    }
+
+    @Test()
+    void testSetLastTask() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Method method = Worker.class.getDeclaredMethod("setLastTask", Task.class);
+        method.setAccessible(true);
+
+        Task task = Task.generateRandomTask();
+
+        method.invoke(worker, task);
+
+        assertEquals(task.getName(), worker.getLastTask());
     }
 }
