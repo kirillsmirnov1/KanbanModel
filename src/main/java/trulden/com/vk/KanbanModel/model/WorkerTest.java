308b3d1ef20db526b1247541a1eab55ceea6f964
trulden <thegaldren@gmail.com>
2019-04-03 16:23:27 +0300

    add "before all" method

--- src/main/java/trulden/com/vk/KanbanModel/model/WorkerTest.java
+++ src/main/java/trulden/com/vk/KanbanModel/model/WorkerTest.java
@@ -7,6 +7,13 @@ import static org.junit.Assert.assertEquals;
 
 class WorkerTest {
 
+    static Worker worker;
+
+    @BeforeAll
+    static void initWorker(){
+        worker = Worker.generateRandomWorker("worker");
+    }
+
     @Test()
     void testCorrectName(){
         String name = "name";
