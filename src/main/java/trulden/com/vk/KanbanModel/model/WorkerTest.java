b9c3ae2332ccaf3d357638c09198fa12e5ebc057
trulden <thegaldren@gmail.com>
2019-04-03 16:21:38 +0300

    remove expected exception

--- src/main/java/trulden/com/vk/KanbanModel/model/WorkerTest.java
+++ src/main/java/trulden/com/vk/KanbanModel/model/WorkerTest.java
@@ -3,14 +3,10 @@ package trulden.com.vk.KanbanModel.model;
 import org.junit.Rule;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.assertEquals;
 
 class WorkerTest {
-
-    @Rule
-    public ExpectedException expectedException = ExpectedException.none();
 
     @Test()
     void testCorrectName(){
